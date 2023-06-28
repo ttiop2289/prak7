ce2b701f7e09fa23a2248fd9b0421764b00ecc7c
ttiop2289 <127741157+ttiop2289@users.noreply.github.com>
2023-06-28 12:16:36 +0300

    Add files via upload

---
+++ MyProject/burger.js
@@ -0,0 +1,5 @@
+console.log(jQuery().jquery);
+
+$(document).on('click', '#burger', function(){
+    $('nav-menu').toggleClass('active');
+})
---
+++ MyProject/views/login.php
@@ -0,0 +1,21 @@
+<div>
+    <a class="btn btn-light" href="index.html" role="button">Home page</a>
+</div>
+<div class="row no-gutters align-items-center justify-content-center text-center h-100">
+    <div class="col-md-6">
+        <form method="post" class="form-signin">
+            <h1 class="h3 mb-3 font-weight-normal">Sign In</h1>
+            <div class="form-group">
+                <label for="inputEmail" class="sr-only">Login</label>
+                <input id="inputEmail" autocomplete="off" type="text" name="login" placeholder="Login"
+                    class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="inputPassword" class="sr-only">Password</label>
+                <input id="inputPassword" autocomplete="off" type="password" name="password" placeholder="password"
+                    class="form-control">
+            </div>
+            <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+        </form>
+    </div>
+</div>
